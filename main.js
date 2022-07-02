1aad10e6b9ba8826fbf322602089dc15ce15eb98
SaiAmbo <90088973+SaiAmbo@users.noreply.github.com>
2022-07-02 16:11:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+noseX=0;
+noseY=0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+
+function setup() {
+video = createCapture(VIDEO);
+video.size(550, 500);
+
+
+canvas = createCanvas(550, 550);
+canvas.position(560,150);
+
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized!');
+}
+
+function draw() {
+    background('#969A97');
+}
+
+function gotPoses(results)
+{
+    if(results.lenth > 0)
+    {
+        console.log(results);
+    }
+}
